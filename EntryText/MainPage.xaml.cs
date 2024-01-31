9477ce3ca3652be157ef5736c472943aaaf9259f
borrmann <max.borrmann@gmx.net>
2024-01-31 09:28:39 +0000

    Add project files.

---
+++ EntryText/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace EntryText
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+        {
+            base.OnNavigatedTo(args);
+
+            await Task.Delay(2000);
+            E_RedText.Text = "I must be red";
+
+        }
+    }
+
+}
